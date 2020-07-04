7205b675e74b6d26f7fa3818431ed901f81b7acc
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-07-04 23:45:52 +0800

    Custom folder paths

---
+++ codeigniter/app/Views/templates/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Employee Portal</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Icons -->
+  <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+	<link rel="shortcut icon" href="public/codebase/assets/media/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="public/codebase/assets/media/favicons/favicon-192x192.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="public/codebase/assets/media/favicons/apple-touch-icon-180x180.png">
+  <!-- END Icons -->
+
+  <!-- Stylesheets -->
+
+  <!-- Page JS Plugins CSS -->
+  <link rel="stylesheet" href="public/codebase/assets/js/plugins/slick/slick.css">
+  <link rel="stylesheet" href="public/codebase/assets/js/plugins/slick/slick-theme.css">
+
+  <!-- Fonts and Codebase framework -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
+  <link rel="stylesheet" id="css-main" href="public/codebase/assets/css/codebase.min.css">
+
+  <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+  <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+  <!-- END Stylesheets -->
+</head>
+<body>
+
+	<h1><?= esc($title); ?></h1>
